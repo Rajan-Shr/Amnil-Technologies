cfa09aa7a63898aa832c28cfbc31133a532df7fd
Rajan-Shr <rajanstha829@gmail.com>
2025-09-17 10:21:47 +0545

    Counter app using class component

---
+++ Task 7/counter/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Counter App using class component</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
